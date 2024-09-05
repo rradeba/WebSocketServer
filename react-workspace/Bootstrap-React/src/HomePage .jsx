07343ac33ca53894cc4c61cfd85aafc6d25d27a1
rradeba <rradeba@gmail.com>
2024-09-05 13:01:24 -0400

    c1

---
+++ react-workspace/Bootstrap-React/src/HomePage .jsx
@@ -0,0 +1,15 @@
+import React from 'react'; 
+import { Button } from 'react-bootstrap'; 
+
+
+const StartShopping = () => {
+    return(
+        <div>
+            <button variant="primary">
+                Shop Now
+            </button>
+        </div>
+    )
+};
+
+export default StartShopping; 
