737dc23a89425df53b93ddd43992ba216d5f6e09
rradeba <rradeba@gmail.com>
2024-09-06 15:11:03 -0400

    c1

--- react-workspace/Bootstrap-React/src/App.jsx
+++ react-workspace/Bootstrap-React/src/App.jsx
@@ -2,20 +2,19 @@ import React from 'react';
 import { Button } from 'react-bootstrap'; 
 import './App.css';
 
-
 const StartShopping = () => {
     return(
-        <div className = "main">
-            <div className = "card">
+        <div className="main">
+            <div className="card">
                 <h1>Crypto Market</h1>
                 <p>Find new crypto coins in our directory.</p>
-                <img className ="image" src="\crypto.jpg" alt="none" />
-                <Button className = "custom-button mb-1 " >
+                <img className="image" src="/crypto.jpg" alt="Crypto" />
+                <Button className="custom-button mb-2">
                     Shop Now
                 </Button>
             </div>
         </div>
-    )
+    );
 };
 
-export default StartShopping; 
+export default StartShopping;
