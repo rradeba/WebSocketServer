b11502731c89430594baeedd2d06d82060a75b37
rradeba <rradeba@gmail.com>
2024-09-06 15:45:30 -0400

    c1

--- react-workspace/Bootstrap-React/src/main.jsx
+++ react-workspace/Bootstrap-React/src/main.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';  // Import your home page or other components
 import ErrorPage from './ErrorPage';  // Import your error page
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={HomePage} />
-        <Route component={ErrorPage} /> 
-      </Switch>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* Add other routes here */}
+        <Route path="*" element={<ErrorPage />} /> {/* This will catch all undefined routes */}
+      </Routes>
     </Router>
   );
 };
