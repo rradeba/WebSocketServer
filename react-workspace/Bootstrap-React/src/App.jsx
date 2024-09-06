448a327e06c1356f666fd41205f5f1ab211e4860
rradeba <rradeba@gmail.com>
2024-09-06 10:53:32 -0400

    c1

--- react-workspace/Bootstrap-React/src/App.jsx
+++ react-workspace/Bootstrap-React/src/App.jsx
@@ -9,7 +9,7 @@ const StartShopping = () => {
             <div className = "card">
                 <h1>Crypto Market</h1>
                 <p>Find new crypto coins in our directory.</p>
-                <img className ="image " src="crypto.jpg" alt="" />
+                <img className ="image " src="\assets\crypto.jpg" alt="" />
                 <Button className = "custom-button" variant="primary">
                     Shop Now
                 </Button>
