8fb4596f605ccb71c0bc3e5e90027510e75e8b11
rradeba <rradeba@gmail.com>
2024-09-06 11:19:42 -0400

    c1

--- react-workspace/Bootstrap-React/src/App.jsx
+++ react-workspace/Bootstrap-React/src/App.jsx
@@ -10,7 +10,7 @@ const StartShopping = () => {
                 <h1>Crypto Market</h1>
                 <p>Find new crypto coins in our directory.</p>
                 <img className ="image" src="\crypto.jpg" alt="none" />
-                <Button className = "custom-button create-button" variant="primary">
+                <Button className = "custom-button mb-1 " variant="primary">
                     Shop Now
                 </Button>
             </div>
