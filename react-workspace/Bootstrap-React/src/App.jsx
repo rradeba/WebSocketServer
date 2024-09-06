0fd82e877e809184ad62747c5cb153ff90550345
rradeba <rradeba@gmail.com>
2024-09-06 10:31:10 -0400

    c1

--- react-workspace/Bootstrap-React/src/App.jsx
+++ react-workspace/Bootstrap-React/src/App.jsx
@@ -5,7 +5,7 @@ import './App.css';
 
 const StartShopping = () => {
     return(
-        <div class = "main">
+        <div className = "main">
             <Button className = "custom-button" variant="primary">
                 Shop Now
             </Button>
